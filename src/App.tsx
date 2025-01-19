98ab28d58c648db5b581cdb0f182b42a09410760
Dikshit Kapoor <dikshitkapoor@Dikshits-MacBook-Air.local>
2025-01-19 14:02:44 +0530

    Adding more logic for assistant

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@
 import { useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import styles from './components/Input.module.css';
+import bot from "./icons/bot.png";
+import user from "./icons/user.png";
 
 function App() {
   const [question, setQuestion] = useState<string>('');
   const [answer, setAnswer] = useState<string>("");
+  const [role, setRole] = useState<string>("user");
+
   const handleSubmit = async (): Promise<any> => {
     setAnswer("loading the answer...")
     const response: any = await axios.post('https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyDuOiQv1CDVWUlKuGkxI3BmNYMX0dBTXbI', {
@@ -16,17 +21,36 @@ function App() {
       ],
     });
     console.log(response.data)
+    setRole("assistant")
     setAnswer(response["data"]["candidates"][0]["content"]["parts"][0]["text"]);
 
   }
 
 
   return (
-    <div>
-      <h1 className="text-2xl font-bold underline">CHAT AI</h1>
-      <textarea value={question} onChange={(e) => { setQuestion(e.target.value) }} ></textarea>
-      <button onClick={handleSubmit}>Generate Answer</button>
-      <pre className="text-sm/6 text-gray-900">{answer}</pre>
+    <div className="App">
+      <div className="Column">
+      <h1 className="Title">CHAT AI</h1>
+  
+      <div className="Content">
+      <div className={styles.wrapper2}>
+      <div>
+        <img
+          src={role === "assistant" ? bot : user}
+          className={styles.avatar}
+          alt="profile avatar"
+        />
+      </div>
+        </div>
+       {answer}
+        </div>
+      
+      <div className={styles.wrapper}>
+      <input placeholder="Your prompt here..."  className={styles.text} value={question} onChange={(e) => { setQuestion(e.target.value) }} ></input>
+      <button className={styles.btn} onClick={handleSubmit}> Go</button>
+      </div>
+      
+    </div>
     </div>
   )
 }
