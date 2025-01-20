cb4da67035ce43d309d15fb3a83b2438ccfff996
Dikshit Kapoor <dikshitkapoor@Dikshits-MacBook-Air.local>
2025-01-20 09:21:37 +0530

    Adding more logic for vite

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,2 @@
 /// <reference types="vite/client" />
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-    readonly VITE_API_KEY: string
-    // more env variables...
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
