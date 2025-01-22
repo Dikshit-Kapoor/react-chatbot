5ea9f02a7500ee2066657a62f61dd4c157c3f2fc
Dikshit Kapoor <dikshitkkapoor19@gmail.com>
2025-01-22 08:39:33 +0530

    Adding firebase changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,24 @@
 
-import { useState } from 'react';
 import './App.css';
-import axios from 'axios';
-import styles from './components/Input.module.css';
-import bot from "./icons/bot.png";
-import user from "./icons/user.png";
+import Chat from './components/Chat';
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  Navigate
+} from "react-router-dom";
 
 function App() {
-  const [question, setQuestion] = useState<string>('');
-  const [answer, setAnswer] = useState<string>("");
-  const [role, setRole] = useState<string>("user");
-  const API_KEY=import.meta.env.VITE_API_KEY;
-  const API_URL=import.meta.env.VITE_GEMINI_API_URL;
-
-  const handleSubmit = async (): Promise<any> => {
-    setAnswer("loading the answer...")
-    setRole("user")
-    setAnswer(question)
-    const response: any = await axios.post(`${API_URL}?key=${API_KEY}`, {
-      contents: [
-        {
-          parts: [{ text: question }]
-        },
-      ],
-    });
-    console.log(response.data)
-    setRole("assistant")
-    setAnswer(response["data"]["candidates"][0]["content"]["parts"][0]["text"]);
-
-  }
 
 
   return (
-    <div className="App">
-      <div className="Column">
-        <h1 className="Title">CHAT AI</h1>
-
-        <div className="Content">
-          <div className={styles.wrapper2}>
-            <div>
-              <img
-                src={role === "assistant" ? bot : user}
-                className={styles.avatar}
-                alt="profile avatar"
-              />
-            </div>
-            <div> {answer}</div>
-
-          </div>
-
-        </div>
-
-        <div className={styles.wrapper}>
-          <input placeholder="Your prompt here..." className={styles.text} value={question} onChange={(e) => { setQuestion(e.target.value) }} ></input>
-          <button className={styles.btn} onClick={handleSubmit}> Go</button>
-        </div>
-
-      </div>
+    <div >
+      <Router>
+        <Routes>
+          <Route path="/" element={<div>Home page</div>}/>
+          <Route path="/chat" element={<Chat/>}/>
+        </Routes>
+      </Router>
     </div>
   )
 }
--- src/api/generateAnswer.tsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-const GenerateAnswer=async (): Promise<any>=>{
-     const response:any=await axios.post('/posts',{})
-     return response.data;
-}
-
-export default GenerateAnswer
