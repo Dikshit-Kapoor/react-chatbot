43fd1e6480339e20e1d44f2b1c22faabe1cabf98
Dikshit Kapoor <dikshitkkapoor19@gmail.com>
2025-01-25 23:36:23 +0530

    Adding sign in page

---
+++ src/components/AuthForm.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
+import styles from '../styles/authForm.module.css';
+
+const AuthForm=()=>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default AuthForm;
---
+++ src/pages/SigninPage.tsx
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout";
+import AuthForm from "../components/AuthForm";
+
+const SigninPage=()=>{
+    return(
+        <Layout>
+            <div>
+
+            </div>
+        </Layout>
+    )
+}
+
+export default SigninPage;
