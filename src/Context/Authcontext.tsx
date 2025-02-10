1b1d56e9432df42a3dbb2f81875f279f45961584
Dikshit Kapoor <dikshitkkapoor19@gmail.com>
2025-02-10 09:56:22 +0530

    Versioning untracked files

--- src/Context/Authcontext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-import { Authcontext } from "../types/type";
-
-
-export const Usercontext=createContext<Authcontext>({
-    user: null,
-    setUser: ()=>{}
-})
